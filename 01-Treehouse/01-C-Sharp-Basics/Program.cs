181d9e10773fec00e860d5c9bbeb4f051d8c72f4
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-16 22:20:08 -0500

    Prompted user and stored answer

--- 01-Treehouse/01-C-Sharp-Basics/Program.cs
+++ 01-Treehouse/01-C-Sharp-Basics/Program.cs
@@ -2,6 +2,15 @@ class Program
 {
     static void Main()
     {
-
+        //  Prompt the user for minutes exercised
+        System.Console.Write("Enter how many minutes you exercised: ");   //  Displays text to user
+        string entry = System.Console.ReadLine();                         //  Read and return user input until they press return key
+        //  Add minutes exercised to total
+        //  Display total minutes exercised to screen
+        //  Repeat until user quits
     }
 }
+
+//  Compiling and running in Workspaces
+    //  To compile: mcs Program.cs
+     //  To run:     mono Program.exe
