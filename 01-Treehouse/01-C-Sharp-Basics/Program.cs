8c214fb93aaca399d00fe153f105cdb598520af5
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-16 23:04:55 -0500

    Added user entry to running total minutes

--- 01-Treehouse/01-C-Sharp-Basics/Program.cs
+++ 01-Treehouse/01-C-Sharp-Basics/Program.cs
@@ -1,15 +1,18 @@
-using System;     //  Can type Console.Write() rather than System.Console.Write() as long as all classes do not have others with the same names in other namespaces
+using System;           //  Can type Console.Write() rather than System.Console.Write() as long as all classes do not have others with the same names in other namespaces
 
-namespace Treehouse.FitnessFrog
+namespace Treehouse
 {
     class Program
     {
         static void Main()
         {
+            int runningTotal = 0;                                      //  Variables in C# have types
             //  Prompt the user for minutes exercised
             Console.Write("Enter how many minutes you exercised: ");   //  Displays text to user
             string entry = Console.ReadLine();                         //  Read and return user input until they press return key
             //  Add minutes exercised to total
+            int minutes = int.Parse(entry);
+            runningTotal = runningTotal + minutes;
             //  Display total minutes exercised to screen
             Console.WriteLine("You've exercised " + entry + " minutes!");
             //  Repeat until user quits
