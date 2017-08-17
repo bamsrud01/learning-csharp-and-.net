a824c3c230f9fd36a38dfe4415d81e366e4e48e6
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-16 22:30:08 -0500

    Added a namespace

--- 01-Treehouse/01-C-Sharp-Basics/Program.cs
+++ 01-Treehouse/01-C-Sharp-Basics/Program.cs
@@ -1,16 +1,23 @@
-class Program
+using System;     //  Can type Console.Write() rather than System.Console.Write() as long as all classes do not have others with the same names in other namespaces
+
+namespace Treehouse.FitnessFrog
 {
-    static void Main()
+    class Program
     {
-        //  Prompt the user for minutes exercised
-        System.Console.Write("Enter how many minutes you exercised: ");   //  Displays text to user
-        string entry = System.Console.ReadLine();                         //  Read and return user input until they press return key
-        //  Add minutes exercised to total
-        //  Display total minutes exercised to screen
-        //  Repeat until user quits
+        static void Main()
+        {
+            //  Prompt the user for minutes exercised
+            Console.Write("Enter how many minutes you exercised: ");   //  Displays text to user
+            string entry = Console.ReadLine();                         //  Read and return user input until they press return key
+            //  Add minutes exercised to total
+            //  Display total minutes exercised to screen
+            //  Repeat until user quits
+        }
     }
 }
 
+
+
 //  Compiling and running in Workspaces
     //  To compile: mcs Program.cs
-     //  To run:     mono Program.exe
+    //  To run:     mono Program.exe
