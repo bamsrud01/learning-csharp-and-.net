5455797bb0231893a48d8843cdef2e2641637b5c
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-23 20:25:58 -0500

    Added method to check if point is in Map bounds

--- 01-Treehouse/02-C-Sharp-Objects/Map.cs
+++ 01-Treehouse/02-C-Sharp-Objects/Map.cs
@@ -5,10 +5,19 @@ namespace TreehouseDefense
     public readonly int Width;
     public readonly int Height;
 
+    //  Constructor
     public Map(int width, int height)
     {
       Width = width;
       Height = height;
+    }
+
+    //  Return true if point is on map
+    public bool OnMap(Point point)
+    {
+      bool inBounds = point.X >= 0 && point.X < Width && point.Y >= 0 && point.Y < Height;
+
+      return inBounds;
     }
   }
 }
