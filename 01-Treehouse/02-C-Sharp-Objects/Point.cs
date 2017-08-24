2ce0f06801b24339d3246b43b5b74f3f978959c8
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-23 20:53:52 -0500

    Wrote method to calculate distance between points

--- 01-Treehouse/02-C-Sharp-Objects/Point.cs
+++ 01-Treehouse/02-C-Sharp-Objects/Point.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TreehouseDefense
 {
   class Point
@@ -5,10 +7,23 @@ namespace TreehouseDefense
     public readonly int X;
     public readonly int Y;
 
+    //  Constructor
     public Point(int x, int y)
     {
       X = x;
       Y = y;
+    }
+
+    //  Calculate distance between this and coordinates
+    public int DistanceTo(int x, int y)
+    {
+      return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
+    }
+
+    //  Calculate the distance between this and point object
+    public int DistanceTo(Point point)
+    {
+      return DistanceTo(point.X, point.Y);
     }
   }
 }
