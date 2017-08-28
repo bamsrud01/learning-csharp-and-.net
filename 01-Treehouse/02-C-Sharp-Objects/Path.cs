cf37bba98a5ab479288d12b3311883e86db198d9
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-27 19:25:36 -0500

    Began writing Path class

--- 01-Treehouse/02-C-Sharp-Objects/Path.cs
+++ 01-Treehouse/02-C-Sharp-Objects/Path.cs
@@ -2,6 +2,12 @@ namespace TreehouseDefense
 {
   class Path
   {
+    //  This time, keep the fields private for "encapsulation"
+    private readonly MapLocation[] _path;
 
+    public Path(MapLocation[] path)
+    {
+      _path = path;
+    }
   }
 }
