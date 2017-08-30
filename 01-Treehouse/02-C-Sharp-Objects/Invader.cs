d285163583c34ec98bf6cbc1c1832671d59e8918
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-29 19:13:23 -0500

    Wrote getter and setter for Invader class

--- 01-Treehouse/02-C-Sharp-Objects/Invader.cs
+++ 01-Treehouse/02-C-Sharp-Objects/Invader.cs
@@ -2,6 +2,14 @@ namespace TreehouseDefense
 {
   class Invader
   {
+    private MapLocation _location;
 
+    public MapLocation GetLocation() {
+      return _location;
+    }
+
+    public void SetLocation(MapLocation value) {
+      _location = value;
+    }
   }
 }
