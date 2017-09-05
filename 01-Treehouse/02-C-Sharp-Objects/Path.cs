c3b6d0e8f21ef8dd427e5230b61fe36de953d4a7
Barrett Amsrud <barrettamsrud@gmail.com>
2017-09-04 20:23:45 -0500

    Added more properties to Invader and Path classes

--- 01-Treehouse/02-C-Sharp-Objects/Path.cs
+++ 01-Treehouse/02-C-Sharp-Objects/Path.cs
@@ -5,6 +5,8 @@ namespace TreehouseDefense
     //  This time, keep the fields private for "encapsulation"
     private readonly MapLocation[] _path;
 
+    public int Length => _path.Length;
+
     public Path(MapLocation[] path)
     {
       _path = path;
