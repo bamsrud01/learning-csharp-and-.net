d5120d1422e1e8f54dcdc3e2021065c36e03ac17
Barrett Amsrud <barrettamsrud@gmail.com>
2017-09-15 22:49:32 -0500

    Increased Accuracy in SniperTower class

--- 01-Treehouse/03-Intermediate-C-Charp/SniperTower.cs
+++ 01-Treehouse/03-Intermediate-C-Charp/SniperTower.cs
@@ -4,6 +4,8 @@ namespace TreehouseDefense.exe
   {
     protected override int Range { get; } = 3;
 
+    protected override double Accuracy { get; } = 0.8;
+
     public SniperTower(MapLocation location) : base(location)
     {}
   }
